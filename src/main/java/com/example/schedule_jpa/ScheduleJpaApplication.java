1942f38eb39a645e87c96d05987de1fe7cb3fe3d
Seung(emily_the_dev) <emily101304@naver.com(emily101304)>
2025-02-10 15:49:16 +0900

    BaseEntity created

---
+++ src/main/java/com/example/schedule_jpa/ScheduleJpaApplication.java
@@ -0,0 +1,15 @@
+package com.example.schedule_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ScheduleJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleJpaApplication.class, args);
+    }
+
+}
