c154e0620064da735b0802d0b07db6f5a78eccc6
Seung(emily_the_dev) <emily101304@naver.com(emily101304)>
2025-02-10 21:47:12 +0900

    BaseEntity 불필요한 어노테이션 제거

--- src/main/java/com/example/schedule_jpa/entity/BaseEntity.java
+++ src/main/java/com/example/schedule_jpa/entity/BaseEntity.java
@@ -15,10 +15,8 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime modifiedAt;
 }
