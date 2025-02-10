98e95335bb5f4c1a61cdfd21595c639be92eac58
Seung(emily_the_dev) <emily101304@naver.com(emily101304)>
2025-02-10 22:08:57 +0900

    User entity created

---
+++ src/main/java/com/example/schedule_jpa/entity/Schedule.java
@@ -0,0 +1,38 @@
+package com.example.schedule_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "schedule")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Schedule(String username, String title, String content) {
+        this.username = username;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+//    public void setUser(User user) {
+//        this.user = user;
+}
---
+++ src/main/java/com/example/schedule_jpa/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.schedule_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
